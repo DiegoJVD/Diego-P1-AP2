22a719cf2f73e9594d0f66785bd7b31df2ff237a
Diego Valerio <67572551+DiegoJVD@users.noreply.github.com>
2021-06-02 18:27:26 -0400

    Agregando entidad Productos.

---
+++ Diego-P1-AP2/Models/Productos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Diego_P1_AP2.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        public String Descripcion { get; set; }
+
+        public int Existencia { get; set; }
+
+        public float Costo { get; set; }
+
+        public float ValorInventario { get; set; }
+
+    }
+}
