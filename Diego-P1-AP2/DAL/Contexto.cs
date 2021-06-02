4f8c1a97ef4bc24975e51ba461d2e234ba819f0f
Diego Valerio <67572551+DiegoJVD@users.noreply.github.com>
2021-06-01 23:53:14 -0400

    Afregando Contexto.

---
+++ Diego-P1-AP2/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diego_P1_AP2.DAL
+{
+    public class Contexto: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite(@"Data Source=Data\Parcial_1.db");
+        }
+    }
+}
